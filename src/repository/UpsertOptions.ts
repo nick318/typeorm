c1795c4314e5b5f04404c5abf0f3ccb4eb7d8b4b
Nikita Salomatin <nikita.salomatin@kuehne-nagel.com>
2023-12-28 22:08:48 +0200

    build

--- src/repository/UpsertOptions.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Special options passed to Repository#upsert
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export interface UpsertOptions<Entity> {
-    conflictPaths: string[]
-}
--- test/functional/repository/basic-methods/entity/RelationAsPrimaryKey.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, JoinColumn, OneToOne } from "../../../../../src";
-import { Category } from "./Category";
-
-@Entity()
-export class RelationAsPrimaryKey {
-    @OneToOne(() => Category, { primary: true })
-    @JoinColumn()
-    category: Category;
-}
